3d05644c990242bc5b5eb62fe1d3267a598a757d
Lkhsss <likhsss@qq.com>
2024-08-21 23:43:34 +0800

    修改构建脚本

--- src/build.rs
+++ src/build.rs
@@ -1,8 +1,9 @@
 use winres;
 
-#[cfg(target_os = "windows")]
 fn main() {
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("icon.ico");
-    res.compile().unwrap();
+    if !cfg!(target_os = "windows") {
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("icon.ico");
+        res.compile().unwrap();
+    }
 }
